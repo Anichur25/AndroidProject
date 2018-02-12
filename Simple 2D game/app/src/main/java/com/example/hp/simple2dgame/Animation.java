3e349d8e102022b3b88fd33fd71d8da7ec84a95c
Unknown <aniscseru6125@gmail.com>
2018-02-13 00:27:20 +0600

    initial commit

---
+++ Simple 2D game/app/src/main/java/com/example/hp/simple2dgame/Animation.java
@@ -0,0 +1,12 @@
+package com.example.hp.simple2dgame;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class Animation {
+
+
+
+}
---
+++ Simple 2D game/app/src/main/java/com/example/hp/simple2dgame/Obstracle.java
@@ -0,0 +1,52 @@
+package com.example.hp.simple2dgame;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+public class Obstracle implements GameObject {
+
+    private Rect rectangle;
+    private int color;
+    private Rect rectangle2;
+
+    public Obstracle(int rectheight,int color,int startX,int startY,int playerGap)
+    {
+        rectangle = new Rect(0,startY,startX,startY + rectheight);
+        rectangle2 = new Rect(startX + playerGap,startY,Constant.SCREEN_WIDTH,startY + rectheight);
+        this.color = color;
+    }
+
+    public Rect getRectangle()
+    {
+        return rectangle;
+    }
+
+    public void increamentY(float y)
+    {
+        rectangle.top += y;
+        rectangle.bottom += y;
+        rectangle2.top += y;
+        rectangle2.bottom += y;
+
+    }
+
+    public boolean playerCollide(RectPlayer player)
+    {
+        return Rect.intersects(rectangle,player.getRectangle()) || Rect.intersects(rectangle2,player.getRectangle());
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        canvas.drawRect(rectangle,paint);
+        canvas.drawRect(rectangle2,paint);
+    }
+    @Override
+    public void update()
+    {
+
+    }
+}
